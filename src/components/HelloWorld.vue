a1a6064ac06b25b52403042372f71714e1ac7066
Tobias Burger <tobias_burger@hotmail.com>
2021-01-29 14:20:54 +0100

    Should throw errors: * msg is bool, but string given * msg is required

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,7 +38,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: { type: Boolean, required: true }
   },
 });
 </script>
